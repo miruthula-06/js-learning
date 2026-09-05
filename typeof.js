b7a86979d4e40f0e8633f763a0fc052365185a0e
miruthula <miruthuladeventhiran@gmail.com>
2026-09-05 23:50:33 +0530

    Resolved merge conflicts

--- typeof.js
+++ typeof.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 let a="miruthula";
 console.log(a,typeof a);
 let b=true;
 console.log(b,typeof b);
 let c=123456789876540988976568;
-=======
-let a="miruthula";
-console.log(a,typeof a);
-let b=true;
-console.log(b,typeof b);
-let c=123456789876540988976568;
->>>>>>> b79df11d377ac9c877ea99101e3f184bed8526f7
 console.log(c,typeof c);
