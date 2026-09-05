b79df11d377ac9c877ea99101e3f184bed8526f7
Miruthula Deventhiran <miruthuladeventhiran@gmail.com>
2026-09-05 23:21:28 +0530

    Add files via upload

---
+++ logical-operator.js
@@ -0,0 +1,8 @@
+let age=20;
+let hasId=false;
+console.log(age>=20 && hasId);
+let hasGithub=true;
+let hasLinkedIn=false;
+console.log(hasGithub||hasLinkedIn);
+let isLogged=true;
+console.log(!isLogged);
---
+++ ternary-operator.js
@@ -0,0 +1,3 @@
+let age=17;
+let result=age>=18 ? "Major":"Minor";
+console.log(result);
